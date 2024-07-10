527d05de779fea35af42fc9cf3d1942cdcb8171e
Olha Rypich <olha.rypich@gmail.com>
2024-07-10 17:07:51 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const inputList = document.querySelector("ul");
+const listItems = [
+    ...document.querySelectorAll("li")
+];
+function sortList(list) {
+    function convertToNum(str) {
+        return +str.slice(1).split(",").join("");
+    }
+    list.sort((el1, el2)=>{
+        return convertToNum(el2.dataset.salary) - convertToNum(el1.dataset.salary);
+    });
+    inputList.append(...list);
+}
+function getEmployees(list) {
+    return list.map((el)=>({
+            name: el.innerText,
+            ...el.dataset
+        }));
+}
+sortList(listItems);
+getEmployees(listItems);
+
+//# sourceMappingURL=index.f75de5e1.js.map
